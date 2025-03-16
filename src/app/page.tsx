0efd66bd430c221e974516001b6bf02ad55ebf69
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-03-16 10:35:07 -0400

    driver data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,27 @@ interface WeatherData {
     wind_speed: number;
 }
 
+interface DriverData {
+    broadcast_name: string;
+    country_code: string;
+    driver_number: number;
+    first_name: string;
+    full_name: string;
+    headshot_url: string;
+    last_name: string;
+    meeting_key: number;
+    name_acronym: string;
+    session_key: number;
+    team_colour: string;
+    team_name: string;
+}
+
 export default function Home() {
     const [fahrenheit, setFahrenheit] = useState<boolean>(false)
     const [delay, setDelay] = useState<number>(0)
 
     const [weather, setWeather] = useState<WeatherData | null>(null)
+    const [drivers, setDrivers] = useState<DriverData[] | null>(null)
 
     useEffect(() => {
 		// most recent date **will be used in production**
@@ -29,10 +45,14 @@ export default function Home() {
         axios.get(`https://api.openf1.org/v1/weather?session_key=latest&date>=${"2025-03-16T06:00:00.000Z"}`)
         .then(res => setWeather(res.data[0]))
         .catch(err => console.log(err))
-    }, [])
-	if (!weather) return <div className="w-screen h-screen grid place-items-center text-2xl font-semibold bg-black text-white">loading...</div>
 
-	if (weather) return (
+        axios.get(`https://api.openf1.org/v1/drivers?session_key=latest`)
+        .then(res => setDrivers(res.data))
+        .catch(err => console.log(err))
+    }, [])
+	if (!weather || !drivers) return <div className="w-screen h-screen grid place-items-center text-2xl font-semibold bg-black text-white">loading...</div>
+
+	if (weather && drivers) return (
 		<div className="h-screen bg-black p-2 text-white">
 			<div className="flex flex-row gap-2">
 				<button className="bg-neutral-900 border border-neutral-800 rounded-full px-4 py-2 font-mono cursor-pointer hover:bg-neutral-800 duration-150 ease-out" onClick={() => setFahrenheit(prev => !prev)}>toggle measurment</button>
@@ -44,6 +64,13 @@ export default function Home() {
 				<p className="bg-neutral-900 border border-neutral-800 rounded-full px-4 py-2 font-mono">{false && `wind_direction:`} {weather?.wind_direction}°</p>
 				<p className="bg-neutral-900 border border-neutral-800 rounded-full px-4 py-2 font-mono">{false && `wind_speed:`} {weather?.wind_speed} m/s</p>
 			</div>
+            <div className="flex flex-col gap-2">
+                {drivers?.map((driver: DriverData, i: number) => (
+                    <div key={i}>
+                        <p>{driver.first_name}. {driver.last_name[0]}</p>
+                    </div>
+                ))}
+            </div>
 		</div>
 	);
 }
