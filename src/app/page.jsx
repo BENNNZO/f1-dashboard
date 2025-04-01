b93c77aeddb2d081a4d80fea470501220af17a00
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-01 19:33:41 -0400

    added ws connection to root page.tsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,18 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function Home() {
+	useEffect(() => {
+		const ws = new WebSocket("ws://localhost:3001")
+
+		ws.onmessage = (message) => {
+			console.log("ws message")
+		}
+
+		return () => ws.close()
+	}, [])
+
 	return (
 		<div>
 			Hello, World!
