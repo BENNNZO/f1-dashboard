14428a53642e102b1078504c95e33dffde8eac3e
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-02 21:52:19 -0400

    added switch case for data types on websocket connection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,52 @@ export default function Home() {
 		const ws = new WebSocket("ws://localhost:3001")
 
 		ws.onmessage = (message) => {
-			console.log("ws message")
+			const { type, data } = JSON.parse(message.data)
+
+			switch (type) {
+				case "CarData.z":
+					console.log("CarData.z received");
+					break;
+				case "Position.z":
+					console.log("Position.z received");
+					break;
+				case "TopThree":
+					console.log("TopThree received");
+					break;
+				case "TimingStats":
+					console.log("TimingStats received");
+					break;
+				case "TimingAppData":
+					console.log("TimingAppData received");
+					break;
+				case "WeatherData":
+					console.log("WeatherData received");
+					break;
+				case "TrackStatus":
+					console.log("TrackStatus received");
+					break;
+				case "DriverList":
+					console.log("DriverList received");
+					break;
+				case "RaceControlMessages":
+					console.log("RaceControlMessages received");
+					break;
+				case "SessionInfo":
+					console.log("SessionInfo received");
+					break;
+				case "SessionData":
+					console.log("SessionData received");
+					break;
+				case "LapCount":
+					console.log("LapCount received");
+					break;
+				case "TimingData":
+					console.log("TimingData received");
+					break;
+				default:
+					console.log(`Unused type: ${type}`)
+			}
+
 		}
 
 		return () => ws.close()
