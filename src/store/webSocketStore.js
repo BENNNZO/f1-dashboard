fbbcec4398495d6fd81e3615dd091e3056abfcb8
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-02 21:49:17 -0400

    added each f1 data point to the websocket store + update functions + deep merge function

--- src/store/webSocketStore.js
+++ src/store/webSocketStore.js
@@ -1,6 +1,31 @@
 import { create } from "zustand";
+import deepMerge from "../utils/deepMerge"
 
 export const useWebSocketStore = create(set => ({
-    CoordData: "",
-    updateCoordData: (newData) => set(() => ({ CoordData: newData }))
+    carData: "",
+    positionData: "",
+    topThree: {},
+    timingStats: {},
+    timingAppData: {},
+    weatherData: {},
+    trackStatus: {},
+    driverList: {},
+    raceControlMessages: {},
+    sessionInfo: {},
+    sessionData: {},
+    lapCount: {},
+    timingData: {},
+    updateCarData: (newData) => set(() => ({ carData: newData })),
+    updatePositionData: (newData) => set(() => ({ positionData: newData })),
+    updateTopThree: (newData) => set(state => ({ topThree: deepMerge(state.topThree, newData) })),
+    updateTimingStats: (newData) => set(state => ({ timingStats: deepMerge(state.timingStats, newData) })),
+    updateTimingAppData: (newData) => set(state => ({ timingAppData: deepMerge(state.timingAppData, newData) })),
+    updateWeatherData: (newData) => set(state => ({ weatherData: deepMerge(state.weatherData, newData) })),
+    updateTrackStatus: (newData) => set(state => ({ trackStatus: deepMerge(state.trackStatus, newData) })),
+    updateDriverList: (newData) => set(state => ({ driverList: deepMerge(state.driverList, newData) })),
+    updateRaceControlMessages: (newData) => set(state => ({ raceControlMessages: deepMerge(state.raceControlMessages, newData) })),
+    updateSessionInfo: (newData) => set(state => ({ sessionInfo: deepMerge(state.sessionInfo, newData) })),
+    updateSessionData: (newData) => set(state => ({ sessionData: deepMerge(state.sessionData, newData) })),
+    updateLapCount: (newData) => set(state => ({ lapCount: deepMerge(state.lapCount, newData) })),
+    updateTimingData: (newData) => set(state => ({ timingData: deepMerge(state.timingData, newData) }))
 }))
