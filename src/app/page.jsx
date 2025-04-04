d788f98b2d91272b8d51a6d20e6543548ac8c141
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-04 13:58:35 -0400

    added init logic to root page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,69 +25,80 @@ export default function Home() {
 	const updateTimingData = useWebSocketStore(state => state.updateTimingData)
 	const updateCircuitData = useWebSocketStore(state => state.updateCircuitData)
 
+	async function updateData(type, data) {
+
+		switch (type) {
+			case "CarData.z":
+				console.log("CarData.z received");
+				updateCarData(JSON.parse(await decodeZippedBase64(data)))
+				break
+			case "Position.z":
+				console.log("Position.z received")
+				updatePositionData(JSON.parse(await decodeZippedBase64(data)))
+				break
+			case "TopThree":
+				console.log("TopThree received")
+				updateTopThree(data)
+				break
+			case "TimingStats":
+				console.log("TimingStats received")
+				updateTimingStats(data)
+				break
+			case "TimingAppData":
+				console.log("TimingAppData received")
+				updateTimingAppData(data)
+				break
+			case "WeatherData":
+				console.log("WeatherData received")
+				updateWeatherData(data)
+				break
+			case "TrackStatus":
+				console.log("TrackStatus received")
+				updateTrackStatus(data)
+				break
+			case "DriverList":
+				console.log("DriverList received")
+				updateDriverList(data)
+				break
+			case "RaceControlMessages":
+				console.log("RaceControlMessages received")
+				updateRaceControlMessages(data)
+				break
+			case "SessionInfo":
+				console.log("SessionInfo received")
+				updateSessionInfo(data)
+				break
+			case "SessionData":
+				console.log("SessionData received")
+				updateSessionData(data)
+				break
+			case "LapCount":
+				console.log("LapCount received")
+				updateLapCount(data)
+				break
+			case "TimingData":
+				console.log("TimingData received")
+				updateTimingData(data)
+				break
+			default:
+				console.log(`Unused type: ${type}`)
+		}
+	}
+
 	useEffect(() => {
 		const ws = new WebSocket("ws://localhost:3001")
 
 		ws.onmessage = async (message) => {
 			const { type, data } = JSON.parse(message.data)
 
-			switch (type) {
-				case "CarData.z":
-					console.log("CarData.z received");
-					updateCarData(JSON.parse(await decodeZippedBase64(data)))
-					break
-				case "Position.z":
-					console.log("Position.z received")
-					updatePositionData(JSON.parse(await decodeZippedBase64(data)))
-					break
-				case "TopThree":
-					console.log("TopThree received")
-					updateTopThree(data)
-					break
-				case "TimingStats":
-					console.log("TimingStats received")
-					updateTimingStats(data)
-					break
-				case "TimingAppData":
-					console.log("TimingAppData received")
-					updateTimingAppData(data)
-					break
-				case "WeatherData":
-					console.log("WeatherData received")
-					updateWeatherData(data)
-					break
-				case "TrackStatus":
-					console.log("TrackStatus received")
-					updateTrackStatus(data)
-					break
-				case "DriverList":
-					console.log("DriverList received")
-					updateDriverList(data)
-					break
-				case "RaceControlMessages":
-					console.log("RaceControlMessages received")
-					updateRaceControlMessages(data)
-					break
-				case "SessionInfo":
-					console.log("SessionInfo received")
-					updateSessionInfo(data)
-					break
-				case "SessionData":
-					console.log("SessionData received")
-					updateSessionData(data)
-					break
-				case "LapCount":
-					console.log("LapCount received")
-					updateLapCount(data)
-					break
-				case "TimingData":
-					console.log("TimingData received")
-					updateTimingData(data)
-					break
-				default:
-					console.log(`Unused type: ${type}`)
+			if (type === "init") {
+				console.log("INIT")
+				Object.keys(data).forEach(key => {
+					updateData(key, data[key])
+				})
+			} else {
+				updateData(type, data)
 			}
-
 		}
 
 		axios.get(`https://api.multiviewer.app/api/v1/circuits/49/${new Date().getFullYear()}`)
