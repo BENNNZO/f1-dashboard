a5753bac99ae16e6d32ab25296aab6cdd908ad4f
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-04 14:00:18 -0400

    consolidated console log

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,62 +26,50 @@ export default function Home() {
 	const updateCircuitData = useWebSocketStore(state => state.updateCircuitData)
 
 	async function updateData(type, data) {
+		console.log(`${type} data received`)
 
 		switch (type) {
 			case "CarData.z":
-				console.log("CarData.z received");
 				updateCarData(JSON.parse(await decodeZippedBase64(data)))
 				break
 			case "Position.z":
-				console.log("Position.z received")
 				updatePositionData(JSON.parse(await decodeZippedBase64(data)))
 				break
 			case "TopThree":
-				console.log("TopThree received")
 				updateTopThree(data)
 				break
 			case "TimingStats":
-				console.log("TimingStats received")
 				updateTimingStats(data)
 				break
 			case "TimingAppData":
-				console.log("TimingAppData received")
 				updateTimingAppData(data)
 				break
 			case "WeatherData":
-				console.log("WeatherData received")
 				updateWeatherData(data)
 				break
 			case "TrackStatus":
-				console.log("TrackStatus received")
 				updateTrackStatus(data)
 				break
 			case "DriverList":
-				console.log("DriverList received")
 				updateDriverList(data)
 				break
 			case "RaceControlMessages":
-				console.log("RaceControlMessages received")
 				updateRaceControlMessages(data)
 				break
 			case "SessionInfo":
-				console.log("SessionInfo received")
 				updateSessionInfo(data)
 				break
 			case "SessionData":
-				console.log("SessionData received")
 				updateSessionData(data)
 				break
 			case "LapCount":
-				console.log("LapCount received")
 				updateLapCount(data)
 				break
 			case "TimingData":
-				console.log("TimingData received")
 				updateTimingData(data)
 				break
 			default:
-				console.log(`Unused type: ${type}`)
+				console.log(`Unused type found: ${type}`)
 		}
 	}
 
