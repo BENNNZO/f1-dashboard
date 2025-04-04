057bf14e1abebf2ab22d71a6379668f697b7f1f0
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-04 14:01:26 -0400

    removed logs from lapCount

--- src/components/LapCount.tsx
+++ src/components/LapCount.tsx
@@ -8,9 +8,6 @@ interface ILapCount {
 export default function LapCount() {
     const lapCount: ILapCount = useWebSocketStore(state => state.lapCount)
 
-    console.log(`\n\n\n\n`)
-    console.log(lapCount)
-
     return (
         <div>
             <p>{lapCount.CurrentLap}</p>
