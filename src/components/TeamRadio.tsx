ec83c728cbb7c091168bdb4dcc275ef230d17118
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-07 19:57:40 -0400

    added back left side border to team radio

--- src/components/TeamRadio.tsx
+++ src/components/TeamRadio.tsx
@@ -11,18 +11,14 @@ interface ICapture {
 }
 
 export default function TeamRadio() {
+    const [playing, setPlaying] = useState(null)
+
     const teamRadio: Record<string, ICapture[]> = useWebSocketStore(state => state.teamRadio)
     const sessionInfo = useWebSocketStore(state => state.sessionInfo)
     const driverList = useWebSocketStore(state => state.driverList)
 
-    console.log(driverList)
-
-    const [playing, setPlaying] = useState(null)
-
-    console.log(teamRadio)
-
     if (teamRadio) return (
-        <div className="flex flex-col gap-2 overflow-y-scroll overflow-x-hidden p-2 border-t">
+        <div className="flex flex-col gap-2 overflow-y-scroll overflow-x-hidden p-2 border-l border-white/10">
             {teamRadio.Captures.reverse().map((capture: ICapture, index: number) => (
                 <AudioPlayer key={index} driver={driverList[capture.RacingNumber]} playing={playing} setPlaying={setPlaying} src={`https://livetiming.formula1.com/static/${sessionInfo.Path}${capture.Path}`} />
             ))}
