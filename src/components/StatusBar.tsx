6b64330341d7f0dddc2b6a59809f7f568b191835
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-07 17:07:07 -0400

    created trackstatus component

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,16 +1,21 @@
 import Weather from "./Weather"
 import SessionInformation from "./SessionInformation"
 import LapCount from "./LapCount"
+import TrackStatus from "./TrackStatus"
 
 export default function StatusBar() {
     return (
         <div className={`flex justify-between border-b border-white/10 p-2`}>
             <div className="flex gap-2">
-                <Weather />
-                <div className="w-px bg-white/10 my-1"></div>
                 <SessionInformation />
+                <div className="w-px bg-white/10 my-1"></div>
+                <Weather />
             </div>
-            <LapCount />
+            <div className="flex gap-2">
+                <LapCount />
+                <div className="w-px bg-white/10 my-1"></div>
+                <TrackStatus />
+            </div>
         </div>
     )
 }
---
+++ src/components/TrackStatus.tsx
@@ -0,0 +1,18 @@
+import { useWebSocketStore } from "@/store/webSocketStore"
+
+interface IStatus {
+    Status: string,
+    Message: string
+}
+
+const statusColors: Record<string, string> = {
+    "1": "rgb(13, 186, 131)"
+}
+
+export default function TrackStatus() {
+    const trackStatus: IStatus = useWebSocketStore(state => state.trackStatus)
+
+    if (trackStatus) return (
+        <p className="px-4 py-1 h-8 rounded-full bg-zinc-900" style={{ background: `${statusColors[trackStatus.Status]}` }}>{trackStatus.Message}</p>
+    )
+}
