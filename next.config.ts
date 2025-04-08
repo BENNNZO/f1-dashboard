cc2c1125cb45c58b71dbda94b8a9ac0f1ecb3350
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-08 11:47:53 -0400

    added deployment files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone',
+    env: {
+        NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL
+    }
 };
 
 export default nextConfig;
+
