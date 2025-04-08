f0e3b0ae3fe757e1d8ef2cf98adbbcd6169601ef
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-08 10:04:55 -0400

    created track status type and typed out strack status component

--- src/components/TrackStatus.tsx
+++ src/components/TrackStatus.tsx
@@ -1,16 +1,12 @@
 import { useWebSocketStore } from "@/store/webSocketStore"
-
-interface IStatus {
-    Status: string,
-    Message: string
-}
+import { ITrackStatus } from "@/types/ITrackStatus.type"
 
 const statusColors: Record<string, string> = {
     "1": "rgb(13, 186, 131)"
 }
 
 export default function TrackStatus() {
-    const trackStatus: IStatus = useWebSocketStore(state => state.trackStatus)
+    const trackStatus: ITrackStatus = useWebSocketStore(state => state.trackStatus)
 
     if (trackStatus) return (
         <p className="px-4 py-1 h-8 rounded-full bg-zinc-900 border border-white/5" style={{ background: `${statusColors[trackStatus.Status]}` }}>{trackStatus.Message}</p>
