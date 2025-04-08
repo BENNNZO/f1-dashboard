ccacdc81587c5def3114700ae20d7419535466c0
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-08 11:09:51 -0400

    converted decode util to typescript

--- src/utils/decodeZipClient.ts
+++ src/utils/decodeZipClient.ts
@@ -1,6 +1,6 @@
 import { inflateRaw } from "pako";
 
-export default async function decodeZippedBase64(data) {
+export default async function decodeZippedBase64(data: any) {
     const buffer = Uint8Array.from(atob(data), c => c.charCodeAt(0));
     const result = inflateRaw(buffer);
     return new TextDecoder().decode(result);
