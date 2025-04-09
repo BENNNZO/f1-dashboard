0086ccc683c901bc7bc51fe5ba7743752474cad4
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-09 19:59:38 -0400

    added skeleton to radio messages

--- src/components/TeamRadio.tsx
+++ src/components/TeamRadio.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react"
 import { useWebSocketStore } from "@/store/webSocketStore"
 import AudioPlayer from "./AudioPlayer"
+import Skeleton from "./Skeleton"
 
 import { ITeamRadio, ICapture } from "@/types/ITeamRadio.type"
 import { ISessionInfo } from "@/types/ISessionInfo.type"
 import { IDriverList } from "@/types/IDriverList.type"
+
 
 export default function TeamRadio() {
     const [playing, setPlaying] = useState<string | null>(null)
@@ -15,18 +17,26 @@ export default function TeamRadio() {
     const sessionInfo: ISessionInfo | null = useWebSocketStore(state => state.sessionInfo)
     const driverList: IDriverList | null = useWebSocketStore(state => state.driverList)
 
-    if (teamRadio && sessionInfo && driverList) return (
+    if (teamRadio && sessionInfo && driverList) {
+        return (
+            <div className="flex flex-col gap-2 overflow-y-scroll overflow-x-hidden p-2 lg:border-l border-t border-white/10">
+                {Object.values(teamRadio.Captures).slice().reverse().map((capture: ICapture, index: number) => {
+                    const driver = driverList[capture.RacingNumber]
+    
+                    return (
+                        <div key={index} className={`flex flex-row items-center gap-3 p-2 pr-6 duration-300 border border-white/5 rounded-xl bg-zinc-900`}>
+                            <p className="text-sm leading-none text-white px-1 font-bold py-1 rounded-md" style={{ background: `#${driver.TeamColour}` }}>{driver.Tla}</p>
+                            <AudioPlayer playing={playing ?? ""} setPlaying={setPlaying} src={`https://livetiming.formula1.com/static/${sessionInfo.Path}${capture.Path}`} />
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    } else return (
         <div className="flex flex-col gap-2 overflow-y-scroll overflow-x-hidden p-2 lg:border-l border-t border-white/10">
-            {Object.values(teamRadio.Captures).slice().reverse().map((capture: ICapture, index: number) => {
-                const driver = driverList[capture.RacingNumber]
-
-                return (
-                    <div key={index} className={`flex flex-row items-center gap-3 p-2 pr-6 duration-300 border border-white/5 rounded-xl bg-zinc-900`}>
-                        <p className="text-sm leading-none text-white px-1 font-bold py-1 rounded-md" style={{ background: `#${driver.TeamColour}` }}>{driver.Tla}</p>
-                        <AudioPlayer playing={playing ?? ""} setPlaying={setPlaying} src={`https://livetiming.formula1.com/static/${sessionInfo.Path}${capture.Path}`} />
-                    </div>
-                )
-            })}
+            {[...Array(20)].map((e, i) => (
+                <Skeleton key={i} delay={i * 100} width={null} height={42} />
+            ))}
         </div>
-    )
+    ) 
 }
