4f8a778d6f4c8719a3117d546d8665e72620776c
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-09 19:56:56 -0400

    added skeleton to control messages

--- src/components/ControlMessages.tsx
+++ src/components/ControlMessages.tsx
@@ -2,6 +2,8 @@ import { useWebSocketStore } from "@/store/webSocketStore"
 
 import { IRaceControlMessages, IMessageOther, IMessageFlag, IMessageDrs } from "@/types/IRaceControlMessages.type"
 
+import Skeleton from "./Skeleton"
+
 const flagTypes = {
     "CHEQUERED": "🏁",
     "BLUE": "🟦",
@@ -13,33 +15,41 @@ const flagTypes = {
 export default function ControlMessages() {
     const raceControlMessages: IRaceControlMessages | null = useWebSocketStore(state => state.raceControlMessages)
 
-    if (raceControlMessages) return (
+    if (raceControlMessages) {
+        return (
+            <div className="flex flex-col gap-2 p-2 h-full overflow-y-scroll overflow-x-hidden border-t border-white/10">
+                {Object.values(raceControlMessages.Messages).slice().reverse().map((message: (IMessageOther | IMessageFlag | IMessageDrs), index: number) => {
+                    const { Message, Category, Utc } = message
+    
+                    const hour = new Date(Utc).getHours() < 10 ? `0${new Date(Utc).getHours()}` : new Date(Utc).getHours()
+                    const minute = new Date(Utc).getMinutes() < 10 ? `0${new Date(Utc).getMinutes()}` : new Date(Utc).getMinutes()
+    
+                    if (Category !== "Flag") {
+                        return (
+                            <div key={index} className="bg-zinc-900 border border-white/5 rounded-xl flex flex-col px-3 py-2">
+                                <p className="text-sm text-zinc-400">{hour}:{minute} - {Category}</p>
+                                <p className="">{Message}</p>
+                            </div>
+                        )
+                    } else {
+                        const { Flag } = message as { Flag: keyof typeof flagTypes }
+    
+                        return (
+                            <div key={index} className="bg-zinc-900 border border-white/5 rounded-xl flex flex-col px-3 py-2">
+                                <p className="text-sm text-zinc-400">{hour}:{minute} - {Category}</p>
+                                <p className="">{flagTypes[Flag]} {Message}</p>
+                            </div>
+                        )
+                    }
+    
+                })}
+            </div>
+        ) 
+    } else return (
         <div className="flex flex-col gap-2 p-2 h-full overflow-y-scroll overflow-x-hidden border-t border-white/10">
-            {Object.values(raceControlMessages.Messages).slice().reverse().map((message: (IMessageOther | IMessageFlag | IMessageDrs), index: number) => {
-                const { Message, Category, Utc } = message
-
-                const hour = new Date(Utc).getHours() < 10 ? `0${new Date(Utc).getHours()}` : new Date(Utc).getHours()
-                const minute = new Date(Utc).getMinutes() < 10 ? `0${new Date(Utc).getMinutes()}` : new Date(Utc).getMinutes()
-
-                if (Category !== "Flag") {
-                    return (
-                        <div key={index} className="bg-zinc-900 border border-white/5 rounded-xl flex flex-col px-3 py-2">
-                            <p className="text-sm text-zinc-400">{hour}:{minute} - {Category}</p>
-                            <p className="">{Message}</p>
-                        </div>
-                    )
-                } else {
-                    const { Flag } = message as { Flag: keyof typeof flagTypes }
-
-                    return (
-                        <div key={index} className="bg-zinc-900 border border-white/5 rounded-xl flex flex-col px-3 py-2">
-                            <p className="text-sm text-zinc-400">{hour}:{minute} - {Category}</p>
-                            <p className="">{flagTypes[Flag]} {Message}</p>
-                        </div>
-                    )
-                }
-
-            })}
+            {[...Array(10)].map((e, i) => (
+                <Skeleton delay={i * 100} width={null} height={Math.random() > 0.5 ? 86 : 62} />
+            ))}
         </div>
     )
 }
--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,7 +1,7 @@
 export default function Skeleton({ width, height, delay }: { width: number | null, height: number | null, delay: number | null }) {
     return (
         <div 
-            className={`bg-white/20 animate-pulse rounded-lg`}
+            className="bg-white/20 animate-pulse rounded-lg shrink-0"
             style={{
                 width: width ? `${width}px` : "100%",
                 height: height ? `${height}px` : "100%",
