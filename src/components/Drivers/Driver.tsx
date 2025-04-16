5f415a06b290328ba452acbe2bb9fdf122439d7d
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-04-15 20:33:24 -0400

    ported code to components

--- src/components/Drivers/Driver.tsx
+++ src/components/Drivers/Driver.tsx
@@ -2,17 +2,9 @@ import Image from "next/image"
 
 import { ITimingDataLine } from "@/types/ITimingData.type"
 import { IDriver } from "@/types/IDriverList.type"
-import { ISector, ISegment } from "@/types/ITimingData.type"
 
 import DriverMetadata from "./DriverMetadata"
-
-const STATUS_COLORS = {
-    "0": "#ffffff40",   // grey
-    "2048": "#fcf41b",  // yellow
-    "2049": "#67ea67",  // green
-    "2051": "#9567ea",  // purple
-    "2064": "red"       // idk what this number is for
-}
+import Sectors from "./Sectors"
 
 export default function Driver({ timingData, tireCompound, driver }: { timingData: ITimingDataLine, tireCompound: string, driver: IDriver }) {
         
@@ -29,21 +21,7 @@ export default function Driver({ timingData, tireCompound, driver }: { timingDat
         >
             <DriverMetadata timingData={timingData} tireCompound={tireCompound} driver={driver} />
             <div className="bg-white/10 h-px w-full block min-[600px]:hidden md:block xl:hidden"></div>
-            <div className="flex gap-2">
-                {timingData.Sectors && Object.values(timingData.Sectors).map((sector: ISector, index: number) => (
-                    <div key={index} className="flex flex-col gap-2 justify-between">
-                        <div className="flex gap-0.5">
-                            {sector.Segments && Object.values(sector.Segments).map((segment: ISegment, index: number) => (
-                                <div key={index} style={{ background: `${STATUS_COLORS[String(segment.Status) as keyof typeof STATUS_COLORS]}` }} className="w-2.5 min-[600]:w-1.5 min-[650px]:w-2 min-[700px]:w-2.5 md:w-3 2xl:w-4 h-1.5 rounded-full duration-150"></div>
-                            ))}
-                        </div>
-                        <div className="flex gap-1 items-end">
-                            <p className={`font-semibold leading-none ${sector.OverallFastest ? "text-purple-400" : sector.PersonalFastest ? "text-green-300" : "text-white"}`}>{sector.PreviousValue === "" ? "---" : sector.PreviousValue}</p>
-                            {/* <p className="font-semibold leading-none text-sm text-white/50">{sector.Value === "" ? "---" : sector.Value}</p> */}
-                        </div>
-                    </div>
-                ))}
-            </div>
+            <Sectors sectorData={timingData.Sectors} />
         </div>
     )
 }
--- src/components/Drivers/Sectors.tsx
+++ src/components/Drivers/Sectors.tsx
@@ -1,7 +1,29 @@
-export default function Sectors() {
+import { ISector, ISegment } from "@/types/ITimingData.type"
+
+const STATUS_COLORS = {
+    "0": "#ffffff40",   // grey
+    "2048": "#fcf41b",  // yellow
+    "2049": "#67ea67",  // green
+    "2051": "#9567ea",  // purple
+    "2064": "red"       // idk what this number is for
+}
+
+export default function Sectors({ sectorData }: { sectorData: ISector[] }) {
     return (
-        <div>
-            
+        <div className="flex gap-2">
+            {sectorData && Object.values(sectorData).map((sector: ISector, index: number) => (
+                <div key={index} className="flex flex-col gap-2 justify-between">
+                    <div className="flex gap-0.5">
+                        {sector.Segments && Object.values(sector.Segments).map((segment: ISegment, index: number) => (
+                            <div key={index} style={{ background: `${STATUS_COLORS[String(segment.Status) as keyof typeof STATUS_COLORS]}` }} className="w-2.5 min-[600]:w-1.5 min-[650px]:w-2 min-[700px]:w-2.5 md:w-3 2xl:w-4 h-1.5 rounded-full duration-150"></div>
+                        ))}
+                    </div>
+                    <div className="flex gap-1 items-end">
+                        <p className={`font-semibold leading-none ${sector.OverallFastest ? "text-purple-400" : sector.PersonalFastest ? "text-green-300" : "text-white"}`}>{sector.PreviousValue === "" ? "---" : sector.PreviousValue}</p>
+                        {/* <p className="font-semibold leading-none text-sm text-white/50">{sector.Value === "" ? "---" : sector.Value}</p> */}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
