2fce8a12c8681e744f50cf2fc4b45839c7546d46
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-28 14:06:47 -0400

    added framer motion to the driver list

--- src/components/Drivers/DriverList.tsx
+++ src/components/Drivers/DriverList.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image"
-
 import { useWebSocketStore } from "@/store/webSocketStore"
+import { motion } from "framer-motion"
 
 import { IDriverList } from "@/types/IDriverList.type"
 import { ITimingAppData } from "@/types/ITimingAppData.type"
@@ -19,19 +18,26 @@ export default function DriverList() {
         // sortedTimingData.unshift(sortedTimingData[0])
 
         return (
-            <div className="p-2 shrink-0 relative overflow-y-scroll flex flex-col gap-2 font-mono">
+            <motion.div
+                className="relative flex flex-col gap-2 p-2 overflow-y-scroll font-mono shrink-0"
+            >
                 {sortedTimingData.map((data, index) => {
                     const driverNumber: string = data[0]
                     const timingData: ITimingDataLine = data[1]
 
                     return (
-                        <Driver key={driverNumber} timingData={timingData} tireCompound={timingAppData?.Lines?.[driverNumber]?.Stints?.[0]?.Compound?.toLowerCase() || "unknown"} driver={driverList[driverNumber]} />
+                        <motion.div
+                            layout
+                            key={driverNumber}
+                        >
+                            <Driver timingData={timingData} tireCompound={timingAppData?.Lines?.[driverNumber]?.Stints?.[0]?.Compound?.toLowerCase() || "unknown"} driver={driverList[driverNumber]} />
+                        </motion.div>
                     )
                 })}
-            </div>
+            </motion.div>
         )
     } else return (
-        <div className="p-2 shrink-0 relative overflow-y-scroll flex flex-col gap-2 w-1/2">
+        <div className="relative flex flex-col gap-2 p-2 w-1/2 overflow-y-scroll shrink-0">
             {[...Array(20)].map((e, i) => (
                 <Skeleton key={i} delay={i * 100} height={50} />
             ))}
